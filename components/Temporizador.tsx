3ef221c7337396e6760d23826e9015df97f868f3
Alessandro Pereira Rodrigues <ale.per.rod@gmail.com>
2022-02-28 19:52:41 -0300

    quase lá

---
+++ components/Temporizador.tsx
@@ -0,0 +1,31 @@
+import styles from '../styles/Temporizador.module.css'
+import {CountdownCircleTimer, useCountdown} from 'react-countdown-circle-timer'
+
+interface TemporizadorProps {
+    duracao: number
+    tempoEsgotado: () => void
+}
+
+export default function Temporizador(props: TemporizadorProps){
+
+    const { remainingTime } = useCountdown({ isPlaying: true, duration: 120, colors: 'red' })
+
+    return (
+        <div className={styles.temporizador}>
+            <CountdownCircleTimer 
+                duration={props.duracao}
+                size={120}
+                isPlaying
+                onComplete={props.tempoEsgotado}
+                colors={[
+                    '#BCE596',
+                    '#F7B801',
+                    '#ED827A'
+                ]}
+                colorsTime={[0,40,80]}
+            >
+                {({remainingTime}) => remainingTime}
+            </CountdownCircleTimer>
+            </div>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,59 @@
-import Questao from '../components/Questao'
+import { useEffect, useState } from 'react'
+import Questionario from '../components/Questionario'
 import QuestaoModel from '../model/questao'
 import RespostaModel from '../model/resposta'
 
+const questaoMock = new QuestaoModel(1,'Melhor cor?',[
+  RespostaModel.errada('Verde'),
+  RespostaModel.errada('Vermelha'),
+  RespostaModel.errada('Azul'),
+  RespostaModel.certa('Preta')
+])
+
 
 export default function Home() {
-  const questaoTeste = new QuestaoModel(1,'Melhor cor?',[
-    RespostaModel.errada('Verde'),
-    RespostaModel.errada('Vermelha'),
-    RespostaModel.errada('Azul'),
-    RespostaModel.certa('Preta')
-  ])
+  const BASE_URL = 'http://localhost:3000/api'
+  const [idsDasQuestoes, setIdsDasQuestoes] = useState<number[]>([])
+  const [questao,setQuestao] = useState<QuestaoModel>(questaoMock)
+  const [respostasCertas,setRespostasCertas] = useState<number>(0)
+
+  async function carregarIdsDasQquestoes() {
+    const resp = await fetch(`${BASE_URL}/questionario`)
+    const idsDasQuestoes = await resp.json()
+    setIdsDasQuestoes(idsDasQuestoes)
+  }
+
+  async function carregarQquestao(idQuestao: number) {
+    const resp = await fetch(`${BASE_URL}/questoes/${idQuestao}`)
+    const json = await resp.json()
+    const novaQuestao = QuestaoModel.criarUsandoObjeto(json)
+    setQuestao(novaQuestao)
+  }
+
+   useEffect(() => {
+    carregarIdsDasQquestoes()
+  },[])
+
+  useEffect(() => {
+    idsDasQuestoes.length > 0 && carregarQquestao(idsDasQuestoes[0])
+  },[idsDasQuestoes])
+
+  function questaoRespondida(questaoRespondida: QuestaoModel){
+    setQuestao(questaoRespondida)
+    const acertou = questaoRespondida.acertou
+    setRespostasCertas(respostasCertas + (acertou ? 1 : 0))
+  }
+
+  function irPraProximoPasso(){
+
+  }
+
   return (
-    <Questao valor={questaoTeste}/>
+      <Questionario
+        questao={questao}
+        ultima={false}
+        questaoRespondida={questaoRespondida}
+        irPraProximoPasso={irPraProximoPasso}
+      />
   )
 }
